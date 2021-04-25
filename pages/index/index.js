a55041b63735e9af0516f96573a3948012131568
Rrrachael <lsrrbc@163.com>
2021-04-25 19:38:08 +0800

    A1B1C1

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,13 @@ const app = getApp()
 
 Page({
   data: {
-    TabCur: 0
+    TabCur: 0,
+    demand_title: "VIVA生命画像馆——周边产品设计",
+    tag1:"教育升级",
+    tag2:"文化保育",
+    imgurl:"/image/diary.png",
+    ngo_name:"益科技",
+    location:"上海/线下"
   },
   // 事件处理函数
   
@@ -13,4 +19,9 @@ Page({
       TabCur: e.currentTarget.dataset.id,
     })
   },
+  detail(){
+    wx.navigateTo({
+      url: '/pages/logs/logs',
+    })
+  }
 })
