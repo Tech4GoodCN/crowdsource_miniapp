88df94a8e537e2826cdae432f49d488a248ca7a1
leo4life2 <leo4life@163.com>
2021-05-05 21:51:07 +0800

    logs page fixed, me favorites WIP

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,8 +3,8 @@
 const app = getApp()
 const AV = require('../../libs/av-core-min.js');
 const getDataForRender = requirementList => requirementList.toJSON();
+
 Page({
-  requirementList:[],
   data: {
     TabCur: 0,
     req_objId: "loremipsum",
@@ -25,14 +25,18 @@ Page({
     wx.showLoading({
       title: '加载中',
     })
+
     const query = new AV.Query('Requirement');
     query.equalTo('requirement_cat'+'.'+'large',this.data.category[this.data.TabCur])
-    query.find().then(requirementList => this.setData({
-      requirementList: requirementList.map(getDataForRender)
-    }))
+    query.find().then((requirementList) => {
+      this.setData({
+        requirementList: requirementList.map(getDataForRender)
+      })
+    })
     .catch(console.error)
     wx.hideLoading()
   },
+
   tabSelect(e) {
     this.setData({
       TabCur: e.currentTarget.dataset.id,
@@ -44,7 +48,6 @@ Page({
     console.log(req_objId)
     wx.navigateTo({
       url: '/pages/logs/logs?req_objId=' + JSON.stringify(req_objId)
-      // url: '/pages/logs/logs?req_objId=' + req_objId
     })
   }
 })
