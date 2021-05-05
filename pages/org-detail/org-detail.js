2c3ff1841fd8d7fc94148c7d3285b6789d48cd57
Reeachel <lsrrbc@163.com>
2021-05-05 21:47:03 +0800

    org_detail requirement

--- pages/org-detail/org-detail.js
+++ pages/org-detail/org-detail.js
@@ -1,13 +1,24 @@
 const app = getApp()
 const AV = require('../../libs/av-core-min.js');
 const util = require('../../utils/util.js');
+const getDataForRender = requirementList => requirementList.toJSON();
+
 Page({
+  requirementList:[],
   data: {
-    isActive: true,
     TabCur: 0,
-    sub: true,
     org_objId:'',
-    organization:{}
+    organization:{},
+    req_objId: "loremipsum",
+    demand_title: "VIVA生命画像馆——周边产品设计",
+    tag1:"教育升级",
+    tag2:"文化保育",
+    imgurl:"/image/diary.png",
+    ngo_name:"益科技",
+    location:"上海/线下",
+    requirementList:[],
+    //改！
+    category:['宣传','教育','技术','调研','设计']
   },
   onLoad:function(options) {
     console.log(options)
@@ -15,6 +26,7 @@ Page({
       org_objId: JSON.parse(options.org_objId)
     })
     this.getOrganizations()
+    this.getRequirement()
   },
   getOrganizations() {
     const query = new AV.Query('Organization');
@@ -24,14 +36,31 @@ Page({
       })
     });
   },
+  getRequirement() {
+    wx.showLoading({
+      title: '加载中',
+    })
+    const query = new AV.Query('Requirement');
+    query.equalTo('requirement_cat'+'.'+'large',this.data.category[this.data.TabCur])
+    query.find().then(requirementList => this.setData({
+      requirementList: requirementList.map(getDataForRender)
+    }))
+    .catch(console.error)
+    wx.hideLoading()
+  },
   tabSelect(e) {
     this.setData({
       TabCur: e.currentTarget.dataset.id,
     })
+    this.getRequirement()
   },
-  detail() {
+  showDetailPage: function(e){
+    var req_objId = e.currentTarget.dataset.req_objid; // need to be all lowercase; i.e. can't be dataset.req_objId
+    console.log(req_objId)
     wx.navigateTo({
-      url: '/pages/logs/logs',
+      url: '/pages/logs/logs?req_objId=' + JSON.stringify(req_objId)
+      // url: '/pages/logs/logs?req_objId=' + req_objId
     })
-  },
+  }
+  
 })
